9c6072a60cde6a78d8f5509748321e2d2e5e3b2c
Alejandro Cortes <Alejandro Cortes>
2016-04-24 23:23:17 -0400

    changed images, added more objects

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
 	var characters = [
-		{
-		name: "Green Arrow", 
-		hp: 100,
-		ap: 10,
-		cp: 30,
-		image: "<img src='assets/images/GW.jpg'>"
-		},
+		{name: "Green Arrow", hp: 100, ap: 12, cp: 28, image: "<img src='assets/images/GW.jpg'>"},
+		{name: "Deadshot", hp: 80, ap: 18, cp: 35, image: "<img src='assets/images/Dshot.jpg'>"},
+		{name: "Deathstroke", hp: 120, ap:16, cp: 33, image: "<img src='assets/images/Dshot.jpg'>"},
+		{name: "Red Hood", hp: 90, ap: 15, cp: 30, image: "<img src='assets/images/Rhood.jpg'>"}
 	];
 });
