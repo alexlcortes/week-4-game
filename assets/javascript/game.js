db1d2ec6e4fc782a07b710cd3fa72541b89b81f7
Alejandro Cortes <Alejandro Cortes>
2016-04-26 00:14:36 -0400

    switched to crystals game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
-	var characters = [
-		{name: "Green Arrow", hp: 100, ap: 12, cp: 28, image: "<img src='assets/images/GW.jpg'>"},
-		{name: "Deadshot", hp: 80, ap: 18, cp: 35, image: "<img src='assets/images/Dshot.jpg'>"},
-		{name: "Deathstroke", hp: 120, ap:16, cp: 33, image: "<img src='assets/images/Dshot.jpg'>"},
-		{name: "Red Hood", hp: 90, ap: 15, cp: 30, image: "<img src='assets/images/Rhood.jpg'>"}
-	];
+	var wins =0;
+	var loses = 0;
+	var randomNumber = [];
+	var gamerNumber = [];
+	var crytalOne = [];
+	var crystalTwo = [];
+	var crystalThree = [];
+	var crystalFour = [];
 
-	 $('button').on("click", function() {
+	  $('button1').on('click', function() {
+	  	
+
 });
