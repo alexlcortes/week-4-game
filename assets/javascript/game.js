e3260c897ac35fdac41edcc2a5ae8a997ee5daf9
Alejandro Cortes <Alejandro Cortes>
2016-04-26 19:13:21 -0400

    added start button, added div with random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
 	var wins =0;
 	var loses = 0;
-	var randomNumber = [];
+	var randomNumber = 0;
 	var gamerNumber = [];
 	var crytalOne = [];
 	var crystalTwo = [];
 	var crystalThree = [];
 	var crystalFour = [];
 
-	  $('button1').on('click', function() {
-	  	
-
+	$('#start').on('click', function() {
+		randomNumber = Math.floor((Math.random() * 100) + 1);
+		console.log(randomNumber);
+		$('#random-number').html(randomNumber);
+	});
 });
+
+
+
+
+
+
