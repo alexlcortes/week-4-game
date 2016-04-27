1c5568d38544f081e5db0e97e61de692b64ceebf
Alejandro Cortes <Alejandro Cortes>
2016-04-26 20:18:57 -0400

    removed old images, added working buttons for all four crystals and things are adding

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,18 +2,53 @@ $(document).ready(function() {
 	var wins =0;
 	var loses = 0;
 	var randomNumber = 0;
-	var gamerNumber = [];
-	var crytalOne = [];
-	var crystalTwo = [];
-	var crystalThree = [];
-	var crystalFour = [];
+	var gamerNumber = 0;
+	var crytalOne = 0;
+	var crystalTwo = 0;
+	var crystalThree = 0;
+	var crystalFour = 0;
 
 	$('#start').on('click', function() {
 		randomNumber = Math.floor((Math.random() * 100) + 1);
 		console.log(randomNumber);
 		$('#random-number').html(randomNumber);
+
+		crystalOne = Math.floor((Math.random() * 20) + 1);
+		console.log(crystalOne);
+
+		crystalTwo = Math.floor((Math.random() * 20) + 1);
+		console.log(crystalTwo);
+
+		crystalThree = Math.floor((Math.random() * 20) + 1);
+		console.log(crystalThree);
+
+		crystalFour = Math.floor((Math.random() * 20) + 1);
+		console.log(crystalFour);
+
+		$('#button1').on('click', function() {
+			gamerNumber = crystalOne + gamerNumber;
+			console.log(gamerNumber);
+		});
+
+		$('#button2').on('click', function() {
+			gamerNumber = crystalTwo + gamerNumber;
+			console.log(gamerNumber);
+		});
+
+		$('#button3').on('click', function() {
+			gamerNumber = crystalThree + gamerNumber;
+			console.log(gamerNumber);
+		});
+
+		$('#button4').on('click', function() {
+			gamerNumber = crystalFour + gamerNumber;
+			console.log(gamerNumber);
+		});
+
 	});
+
 });
+
 
 
 
