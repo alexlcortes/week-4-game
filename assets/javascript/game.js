bbb9167b3772c64d74313842c0dfa79cb14dda06
Alejandro Cortes <Alejandro Cortes>
2016-04-26 23:40:53 -0400

    it kinda works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,83 @@
 $(document).ready(function() {
+
+	// variables
 	var wins =0;
 	var loses = 0;
 	var gamerNumber = 0;
-	var crytalOne = 0;
-	var crystalTwo = 0;
-	var crystalThree = 0;
-	var crystalFour = 0;
 
-		var randomNumber = Math.floor((Math.random() * 100) + 1);
-		console.log(randomNumber);
-		$('#random-number').html(randomNumber);
+	// number generation
+	var randomNumber = Math.floor((Math.random() * 100) + 1);
+	console.log(randomNumber);
+	$('#random-number').html(randomNumber);
 
-		crystalOne = Math.floor((Math.random() * 20) + 1);
-		console.log(crystalOne);
+	var crystalOne = Math.floor((Math.random() * 20) + 1);
+	console.log(crystalOne);
 
-		crystalTwo = Math.floor((Math.random() * 20) + 1);
-		console.log(crystalTwo);
+	var crystalTwo = Math.floor((Math.random() * 20) + 1);
+	console.log(crystalTwo);
 
-		crystalThree = Math.floor((Math.random() * 20) + 1);
-		console.log(crystalThree);
+	var crystalThree = Math.floor((Math.random() * 20) + 1);
+	console.log(crystalThree);
 
-		crystalFour = Math.floor((Math.random() * 20) + 1);
-		console.log(crystalFour);
+	var crystalFour = Math.floor((Math.random() * 20) + 1);
+	console.log(crystalFour);
 
+
+		// buttons
 		$('.button1').on('click', function() {
 			gamerNumber = crystalOne + gamerNumber;
 			$('#gamer-number').html(gamerNumber);
+			check();
 		});
 
 		$('.button2').on('click', function() {
 			gamerNumber = crystalTwo + gamerNumber;
 			$('#gamer-number').html(gamerNumber);
+			check();
 		});
 
 		$('.button3').on('click', function() {
 			gamerNumber = crystalThree + gamerNumber;
 			$('#gamer-number').html(gamerNumber);
+			check();
 		});
 
 		$('.button4').on('click', function() {
 			gamerNumber = crystalFour + gamerNumber;
 			$('#gamer-number').html(gamerNumber);
-
+			check();
 		});
 
+		// resets
+		var newGame = function() {
+			randomNumber = Math.floor((Math.random() * 100) + 1);
+			$('#random-number').html(randomNumber);
+
+			crystalOne = Math.floor((Math.random() * 20) + 1);
+
+			crystalTwo = Math.floor((Math.random() * 20) + 1);
+
+			crystalThree = Math.floor((Math.random() * 20) + 1);
+
+			crystalFour = Math.floor((Math.random() * 20) + 1);
+
+			gamerNumber = 0;
+		}
 
 
+		// statements
+		var check = function() {
+			if (randomNumber == gamerNumber) {
+				wins++;
+				$('#wins').html(wins);
+				newGame();
+			} else if (randomNumber < gamerNumber) {
+				loses++;
+				$('#loses').html(loses);
+				newGame();
+			}
+		}
 	});
-
-// $('#reset').on('click', function() {
-// 	ready();
-// });
 
 
 
